303b7b3efba9063473de6ffb3159dff80ae47690
Klemens Muthmann <klemens.muthmann@cyface.de>
2024-10-10 16:40:01 +0200

    Initial Commit

---
+++ cyfaceapp/cyfaceappApp.swift
@@ -0,0 +1,17 @@
+//
+//  cyfaceappApp.swift
+//  cyfaceapp
+//
+//  Created by Klemens Muthmann on 10.10.24.
+//
+
+import SwiftUI
+
+@main
+struct cyfaceappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ cyfaceappTests/cyfaceappTests.swift
@@ -0,0 +1,17 @@
+//
+//  cyfaceappTests.swift
+//  cyfaceappTests
+//
+//  Created by Klemens Muthmann on 10.10.24.
+//
+
+import Testing
+@testable import cyfaceapp
+
+struct cyfaceappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
